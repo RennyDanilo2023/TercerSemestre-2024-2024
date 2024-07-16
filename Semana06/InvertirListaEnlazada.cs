41cdb93c81690f8fd3342f5a5c96c20e8ad74d5e
RennyDanilo2023 <142845224+RennyDanilo2023@users.noreply.github.com>
2024-07-16 10:14:28 -0500

    Tarea: Semana 06

---
+++ Semana06/InvertirListaEnlazada.cs
@@ -0,0 +1,84 @@
+using System;
+
+public class Node
+{
+    public int Data;
+    public Node Next;
+
+    public Node(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList
+{
+    private Node head;
+
+    public void Add(int data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void Print()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+
+    public void Invert()
+    {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null)
+        {
+            next = current.Next;
+            current.Next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList list = new LinkedList();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+
+        Console.WriteLine("Original List:");
+        list.Print();
+
+        list.Invert();
+
+        Console.WriteLine("Inverted List:");
+        list.Print();
+    }
+}
